0f86788a2149a0c377bebe79d9a790ec9044f585
kasiah00 <saehyun3818@gmail.com>
2024-12-18 16:27:06 +0900

    Time: 3 ms (50.81%), Space: 17.9 MB (11.95%) - LeetHub

---
+++ 1682-most-visited-sector-in-a-circular-track/1682-most-visited-sector-in-a-circular-track.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
+        start = rounds[0]
+        end = rounds[-1]
+        if end>=start:
+            return [i for i in range(start,end+1)]
+        else:
+            return [i for i in range(1,end+1)]+[i for i in range(start,n+1)]
